7cbbc730e4909c972c13b5076f391c9e92f886bc
Pawan Singh <pawansx.1@gmail.com>
2018-04-04 19:35:53 +0530

    Add App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+import React from 'react';
+import counter from '../containers/Counter';
+import addCounter from '../containers/AddCounter';
+import removeCounter from '../containers/RemoveCounter';
+
+const App = () => {
+  return (
+    <div className="container">
+      <counter></counter><br />
+      <div className="columns">
+        <div className="column is-11">
+          <addCounter></addCounter>
+        </div>
+        <div className="column auto">
+          <removeCounter></removeCounter>
+        </div>
       </div>
-    );
-  }
+      </div>
+  )
 }
-
 export default App;
